d6ca9fa4ccfc154c17d28fed5ca76ed0539ed5de
RAVI SHARMA <ravisharma23523@gmail.com>
2023-04-03 23:14:43 +0530

    task 2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+let sum = (a,b)=>{return a+b};
+
+
+console.log(sum(10,20));
+
+const Student = {
+    name : 'ravi sharma',
+    age : 23,
+    greet : ()=>{
+        console.log('hello ' + this.name);
+    }
+}
+Student.greet();
