916b83e1da4c71de542627347c60b16599041092
RAVI SHARMA <ravisharma23523@gmail.com>
2023-04-05 14:14:51 +0530

    task 2

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.use((req,res,next)=>{
-  console.log('In the first middleware');
-  next();
-})
-app.use((req,res,next)=>{
-  console.log('In the second middleware');
-  res.send("<h1>Express js Server</h1>")
-})
+const app = express();
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" placeholder="title" name="title"><input type="text" placeholder="size" name="size"><button type="submit">Add Product</button></form>');
 });
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
