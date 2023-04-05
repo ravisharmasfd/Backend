eb4bc27334b99d3a7ea524c8b51f9b08866bae59
RAVI SHARMA <ravisharma23523@gmail.com>
2023-04-05 13:49:56 +0530

    task 1

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const http = require('http');
-const routes = require('./routes')
-const server = http.createServer(routes);
+const express = require('express')
+const app = express()
 
-server.listen(3000, () => {
+app.use((req,res,next)=>{
+  console.log('In the first middleware');
+  next();
+})
+app.use((req,res,next)=>{
+  console.log('In the second middleware');
+  res.send("<h1>Express js Server</h1>")
+})
+
+app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
