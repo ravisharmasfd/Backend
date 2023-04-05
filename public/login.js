ebe820e0096899decff3e62fc232656436c2c01d
RAVI SHARMA <ravisharma23523@gmail.com>
2023-04-05 20:00:34 +0530

    task 6

--- public/login.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById("username");
-const form = document.getElementById("form");
-form.onsubmit = function (e) {
-  e.preventDefault();
-  const username = input.value;
-  localStorage.setItem("username", username);
-  window.location.href = "/";
-};
--- routes/login.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const dirname = require('../utls/path')
-const express = require('express');
-const router = express.Router();
-router.get('/', (req, res) => {
-    res.sendFile(path.join(dirname,"view","login.html"))
-  });
-module.exports = router
-  
