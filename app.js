f2ba96d0055c4aebec6736f26360ca6caa190a0c
RAVI SHARMA <118259227+ravisharmasfd@users.noreply.github.com>
2023-04-07 13:05:24 +0530

    task 17

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -21,5 +21,5 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+sequelize.sync().then(()=>{app.listen(3000);})
 
-app.listen(3000);
