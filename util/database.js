2a848883d655ce2534dfdf870d2838ab6ba9c8ac
RAVI SHARMA <118259227+ravisharmasfd@users.noreply.github.com>
2023-04-07 12:42:59 +0530

    task 16

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'nothingPass'
+    password: 'nothingDb'
 });
 
 module.exports = pool.promise();
